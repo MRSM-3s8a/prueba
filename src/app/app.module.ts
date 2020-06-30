1d10625f81cccc5ec3b26fea24634cd71be0f2fa
Isaac Yriarte <isaacyagi@gmail.com>
2020-06-30 15:57:37 -0500

    Refactor: Cambio en la importacion de los componentes de ng zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconsProviderModule } from './icons-provider.module';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+import { NgZorroModule } from './modules/ng-zorro/ng-zorro.module';
+
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +14,7 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+
 
 registerLocaleData(en);
 
@@ -24,8 +26,7 @@ registerLocaleData(en);
     BrowserModule,
     AppRoutingModule,
     IconsProviderModule,
-    NzLayoutModule,
-    NzMenuModule,
+    NgZorroModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule
