b909e389904a520c1ab1050a34aebdac12c17abd
Isaac Yriarte <isaacyagi@gmail.com>
2020-06-30 13:35:24 -0500

    Feat: Intento de intregracion frontend enviado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobileNavbarComponent } from './components/navigation/mobile-navbar/mobile-navbar.component';
+import { SideNavbarComponent } from './components/navigation/side-navbar/side-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileNavbarComponent,
+    SideNavbarComponent
   ],
   imports: [
     BrowserModule,
