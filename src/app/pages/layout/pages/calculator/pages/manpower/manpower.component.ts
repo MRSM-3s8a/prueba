aa0377ecacf6d3dcfd19de9eac53c20dc11cd016
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-01 18:55:47 -0500

    Feat: Agrega rutas de manpower

--- src/app/pages/layout/pages/calculator/pages/manpower/manpower.component.ts
+++ src/app/pages/layout/pages/calculator/pages/manpower/manpower.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-manpower',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManpowerComponent implements OnInit {
 
-  constructor() { }
+  menuActivated: string;
+  ALLOWED_ROUTES = [
+    'loaders',
+    'trucks',
+    'jumbos',
+    'longhole',
+    'charger',
+    'forklifts',
+    'lvs',
+    'others',
+  ];
+
+  constructor(
+    public activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe((params) => {
+      const menuOption = params['menuOption'];
+      if (this.ALLOWED_ROUTES.includes(menuOption))
+        this.menuActivated = menuOption;
+      else
+        this.menuActivated = this.ALLOWED_ROUTES[0];
+    });
   }
 
 }
