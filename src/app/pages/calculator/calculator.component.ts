c39708923963a555b0ec679a191ff14ef990d810
Isaac Yriarte <isaacyagi@gmail.com>
2020-06-30 19:43:33 -0500

    Feat: Implementa menu lateral en calculadora

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  openMap: { [name: string]: boolean } = {
+    sub1: true,
+    sub2: false,
+    sub3: false
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openHandler(value: string): void {
+    for (const key in this.openMap) {
+      if (key !== value) {
+        this.openMap[key] = false;
+      }
+    }
   }
 
 }
--- src/app/pages/calculator/calculator.module.ts
+++ src/app/pages/calculator/calculator.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CalculatorRoutingModule } from './calculator-routing.module';
 import { CalculatorComponent } from './calculator.component';
+import { NgZorroModule } from 'src/app/modules/ng-zorro/ng-zorro.module';
 
 
 @NgModule({
   declarations: [CalculatorComponent],
   imports: [
-    CalculatorRoutingModule
+    CalculatorRoutingModule,
+    NgZorroModule
   ],
   exports:[CalculatorComponent]
 })
