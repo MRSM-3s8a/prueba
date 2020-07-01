682188bb3783e3b33674f129037958ef7cbbc666
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-01 14:10:01 -0500

    Refactor: Agrega la welcome page a la navegacion.

--- src/app/pages/layout/layout-routing.module.ts
+++ src/app/pages/layout/layout-routing.module.ts
@@ -9,17 +9,27 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
-          path: '',
-          redirectTo: 'home'
-      },
-      {
           path: 'home',
           loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
       },
       {
           path: 'calculator',
           loadChildren: () => import('./pages/calculator/calculator.module').then(m => m.CalculatorModule)
-      }
+      },
+      {
+          path: 'welcome',
+          loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+      {
+          path: '**',
+          pathMatch: 'full',
+          redirectTo: 'home'
+      },
   ] 
   },
 ];
