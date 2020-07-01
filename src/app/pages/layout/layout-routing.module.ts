7258855036a39ab533bb8001d3f74df11ff645e7
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-01 14:02:56 -0500

    Refactor: Se navegan a las rutas hijas del layout

--- src/app/pages/layout/layout-routing.module.ts
+++ src/app/pages/layout/layout-routing.module.ts
@@ -4,7 +4,24 @@ import { LayoutComponent } from './layout.component';
 
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent },
+  { 
+    path: '', 
+    component: LayoutComponent,
+    children: [
+      {
+          path: '',
+          redirectTo: 'home'
+      },
+      {
+          path: 'home',
+          loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+      },
+      {
+          path: 'calculator',
+          loadChildren: () => import('./pages/calculator/calculator.module').then(m => m.CalculatorModule)
+      }
+  ] 
+  },
 ];
 
 @NgModule({
