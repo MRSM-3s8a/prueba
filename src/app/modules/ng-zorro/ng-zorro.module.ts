91f9744c798bdcaa4a2ccf476efd0bb5fafd99ab
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-02 14:57:01 -0500

    Feat: Creacion inicial de componente equipment-fleet-form

--- src/app/modules/ng-zorro/ng-zorro.module.ts
+++ src/app/modules/ng-zorro/ng-zorro.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
 
 
 @NgModule({
   declarations: [],
   imports: [
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    NzCardModule,
+    NzSliderModule
   ],
   exports: [
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    NzCardModule,
+    NzSliderModule
   ]
 })
 export class NgZorroModule { }
