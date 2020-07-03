1644296d0b79e9e4d97ee004ebd2ef5b67e5fa77
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-03 13:06:42 -0500

    Feat: Agrega la tabla de consumables al proyecto

--- src/app/pages/layout/pages/calculator/pages/consumables/consumables.component.ts
+++ src/app/pages/layout/pages/calculator/pages/consumables/consumables.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsumablesComponent implements OnInit {
 
+  checked = false;
+  indeterminate = false;
+  listOfCurrentPageData: ItemData[] = [];
+  listOfData: ItemData[] = [];
+  setOfCheckedId = new Set<number>();
   constructor() { }
 
   ngOnInit(): void {
+    this.listOfData = new Array(200).fill(0).map((_, index) => {
+      return {
+        id: index,
+        name: `Edward King ${index}`,
+        age: 32,
+        address: `London, Park Lane no. ${index}`
+      };
+    });
   }
 
+  onAllChecked(value: boolean): void {
+    this.listOfCurrentPageData.forEach(item => this.updateCheckedSet(item.id, value));
+    this.refreshCheckedStatus();
+  }
+
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  refreshCheckedStatus(): void {
+    this.checked = this.listOfCurrentPageData.every(item => this.setOfCheckedId.has(item.id));
+    this.indeterminate = this.listOfCurrentPageData.some(item => this.setOfCheckedId.has(item.id)) && !this.checked;
+  }
+
+  onCurrentPageDataChange($event: ItemData[]): void {
+    this.listOfCurrentPageData = $event;
+    this.refreshCheckedStatus();
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+}
+
+export interface ItemData {
+  id: number;
+  name: string;
+  age: number;
+  address: string;
 }
